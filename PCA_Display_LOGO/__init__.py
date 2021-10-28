95e74ade48cf14064eca7bb4a10711997476c96d
FRAG <admin@fragleveldesign.com>
2021-10-29 00:32:24 +0100

    Minor update to flush

--- PCA_Display_LOGO/__init__.py
+++ PCA_Display_LOGO/__init__.py
@@ -1,13 +1,13 @@
 import gc
 
-import defines, rgb, buttons, system
+import defines, display, buttons, system
 from time import sleep
 
 UP, DOWN, LEFT, RIGHT = defines.BTN_UP, defines.BTN_DOWN, defines.BTN_LEFT, defines.BTN_RIGHT
 direction = ''
-B = (0, 0, 0)
-W = (255, 255, 255)
-Y = (255, 128, 0)
+B = [0x000000]
+W = [0xFFFFFF]
+Y = [0xFF9900]
 
 PCA = [
     [Y, Y, Y, Y, Y, B, B, B, B, B, B, B, B, W, W, W, W, W, B, B, B, B, B, B, B, B, B, B, B, Y, Y, Y],
@@ -28,22 +28,14 @@ def input_B(pressed):
 
 
 buttons.register(defines.BTN_B, input_B)
-
-rgb.background()
-rgb.setbrightness(3)
 gc.collect()
 
 while direction != defines.BTN_B:
     gc.collect()
-    rgb.disablecomp()
-    rgb.clear()
     for row_id, row in enumerate(PCA, 0):
         for col_id, pixel in enumerate(row, 0):
-            rgb.pixel(pixel, (col_id, row_id))
+            display.drawPixel(pixel, (col_id, row_id))
 
-    rgb.enablecomp()
-    sleep(0.05)
-    rgb.clear()
-    sleep(0.05)
+    display.flush()
 
 system.reboot()
